3091790413e5b3b060204c3f787ec422d5b43fb6
HardyNuklear <hardy.herinavalona@nuklear.io>
2025-11-03 09:14:13 +0300

    modal design pattern

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import { LargeAuthorListItems } from "./components/author/LargeAuthorListItems";
-import { SmallAuthorListItems } from "./components/author/SmallAuthorListItems";
-import { LargeBooksListItems } from "./components/books/LargeBooksListItems";
 import { SmallBooksListItems } from "./components/books/SmallBooksListItems";
-import { NumberedList } from "./components/lists/NumberedList";
 import { RegularList } from "./components/lists/RegularList";
-import { authors } from "./data/authors";
+import { Modal } from "./components/Modal";
 import { books } from "./data/books";
 
 function App() {
   return (
     <>
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={SmallAuthorListItems}
-      />
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={LargeAuthorListItems}
-      />
-      <hr />
-      <h1>books lists</h1>
-      <RegularList
-        items={books}
-        sourceName={"books"}
-        ItemComponent={LargeBooksListItems}
-      />
-      <NumberedList
-        items={books}
-        sourceName={"books"}
-        ItemComponent={SmallBooksListItems}
-      />
+      <Modal>
+        <RegularList
+          items={books}
+          sourceName={"books"}
+          ItemComponent={SmallBooksListItems}
+        />
+      </Modal>
     </>
   );
 }
